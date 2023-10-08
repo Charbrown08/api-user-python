601f94cd6225b8303bf94f7da6a43af1f9cd8124
Charbrown08 <cartam15@hotmail.com>
2023-10-08 18:33:55 -0500

    add conection with sqlalchemy

--- main.py
+++ main.py
@@ -1,157 +1,104 @@
-from fastapi import FastAPI,Body,Path,Query
-from pydantic import BaseModel,EmailStr,Field
+from fastapi import Depends, FastAPI,Body,Path,Query,HTTPException,Request
+from pydantic import BaseModel,Field
 from typing import Optional,List
-from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse
+from config.database import Session,engine,Base
+from models.user import User as UserModel
+from models.address import Address as AddressModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 app.title = "Api Users"
 app.version = "0.0.1"
 
-
-#Creamos nuestros modelos de direccion de usuarios 
-
-class AddressModel(BaseModel):
-    id:int
-    address_1:str = Field(min_length=5,max_length=50)
-    address_2: str = Field(max_length=50,min_length=5,ge=0,skip=True)
-    city:str=Field(min_length=5,max_length=50)
-    state:str=Field(min_length=5,max_length=50)
-    zip:Optional[int] = None
-    country: str= "Colombia"
-
-    
+Base.metadata.create_all(bind=engine)
+  
 # creamos nuestro modelo de usuario 
 
-class UserModel(BaseModel):
-    id:Optional[int]= None
-    name:str
-    lastname:str
-    age:int
-    email:EmailStr 
-    password:str
-    address:AddressModel
+class UserM(BaseModel):
+    id: Optional[int]= None
+    name: str = Field(min_length=5,max_length=50)
+    lastname: str = Field(min_length=5,max_length=50)
+    age: int = Field(ge=1)
+    email: str = Field(min_length=5,max_length=50)
+    password: str = Field(min_length=5,max_length=50)
+    address: str = Field(min_length=5,max_length=50)
 
-
-
-
-# simulamos datos en una base de datos para retornar  la direccion de un usuario 
-address = [{
-    "id":1,
-    "address_1":"avenida 1° A",
-    "address_2":"avenida 2° A",
-    "city":"Bogota",
-    "state":"Cundinamarca",
-    "zip":"123456",
-    "country":"Colombia"
-},
-{
-    "id":2,
-    "address_1":"avenida 1° B",
-    "address_2":"avenida 2° B",
-    "city":"Lima",
-    "state":"Lima", 
-    "zip":"123432",
-    "country":"Peru"
-},
-{
-    "id":3,
-    "address_1":"avenida 1° c",
-    "address_2":"avenida 2° c",
-    "city":"Lima",
-    "state":"Lima", 
-    "zip":"123432",
-    "country":"Peru"
-}
-]
-
-#
-
-# simulamos datos en una base de datos para retornar  el usuario  
-
-users = [{
-    "id":1,
-    "name":"Juan",
-    "lastname":"Perez",
-    "age":21,
-    "email":"email.com",
-    "password":"password123",
-    "address":address[0]
-
-},{
-    "id":2,
-    "name":"Maria",
-    "lastname":"Gomez",
-    "age":25,
-    "email":"email.com",
-    "password":"password123",
-    "address":address[1]
-}
-,{
-    "id":3,
-    "name":"Juan",
-    "lastname":"Moreno",
-    "age":45,
-    "email":"email2.com",
-    "password":"password123",
-    "address":address[2]
-}
-]
-
+   
 
 
 # Creamos el metodo que nos permitira obtener todos los usuarios
-@app.get("/users",tags=['Users'],response_model= List[UserModel])
-def get_users()-> List[UserModel]:
-    return JSONResponse(status_code=200,content=users)
+@app.get("/users",tags=['Users'],response_model= List[UserM])
+def get_users()-> List[UserM]:
+    db= Session()
+    result= db.query(UserModel).all() 
+    if not result:
+        JSONResponse(status_code=404,content={"message":"users not found"})
+    return JSONResponse(status_code=200,content=jsonable_encoder(result))
+
+
+# Creamos el endopoint que nos permitira obtener un usuario por su id
+
+@app.get("/users/{id}",tags=['Users'],response_model= UserM)
+def get_user_by_id(id: int)-> UserM:
+    db = Session()
+    result = db.query(UserModel).filter(UserModel.id == id).first()
+    if not result:
+       return JSONResponse(status_code=404,content={"message":"user not found"})
+    return JSONResponse(status_code=200,content=jsonable_encoder(result))
 
 
 
-# creamos el endpoint para obtener un usuario por su id en la ruta del endpoint
-@app.get("/users/by/{id}",tags=['Users'],response_model= UserModel)
-def get_user_by_id(id:int)-> UserModel:
-    for user in users:
-        if user["id"] == id:
-            return JSONResponse(status_code=200,content=user)
-    return JSONResponse(status_code=404,content={"message":"user not found"})
+#creamos que nos permitira buscar el usuario por  su direccion 
 
-# Creamos el metodo que nos permitira obtener usuarios por su pais 
+@app.get("/users/address/{address}",tags=['Users'],response_model= List[UserM])
+def get_user_by_address(address: str)-> List[UserM]:
+    db = Session()
+    result = db.query(UserModel).filter(UserModel.address == address).all()
+    if not result:
+        return JSONResponse(status_code=404,content={"message":"user not found"})
+    return JSONResponse(status_code=200,content=jsonable_encoder(result))
 
-@app.get("/users/country/{country}",tags=['Users'],response_model=List[UserModel])
-def get_users_by_country(country:str=Path(min_length=1,max_length=30))->List[UserModel]:
-    userCountry=[user for user in users if user["address"]["country"].lower() == country.lower()]
-    return JSONResponse(status_code=200,content=userCountry)
 
 
 
 #cramos el endpoint que nos permitira crear usuarios 
-@app.post("/users",tags=['Users'], response_model=dict,status_code=201)
-def create_users(user: UserModel)-> dict:
-    users.append(user)
+@app.post("/users",tags=['Users'], response_model= UserM,status_code=201)
+def create_users(user: UserM)-> UserM:
+    db = Session()
+    new_user = UserModel(**user.dict())
+    db.add(new_user)
+    db.commit()
     return JSONResponse(status_code=201,content={"message":"user created succesfully"})
 
-# #creaamos el  endpoint que nos permitira actualizar un usuario por su id
-@app.put("/users/{id}",tags=['Users'], response_model= dict,status_code=200)
-def update_users(
-    id:int,
-    user: UserModel) -> dict:
-    for user in users:
-        if user["id"] == id:
-            user["name"] = user.name
-            user["lastname"] = user.lastname
-            user["age"] = user.age
-            user["email"] = user.email
-            user["password"] = user.password
-            user["address"] = user.address
-            return JSONResponse(status_code=200,content={"message":"user updated"})
-    
-          
-#creamos el endpoint que nos permitira eliminar un usuario por su id
 
-@app.delete("/users/{id}", tags=["Users"], response_model=dict, status_code=200)
-def delete_users(id: int) -> dict:
-    for user in users:
-        if user["id"] == id:
-            users.remove(user)
-            return JSONResponse(
-                status_code=200, content={"message": "Delete user"}
-            )
+# creamos el endpoint que nos permitira actualizar un usuario
+
+@app.put("/users/{id}",tags=['Users'], response_model= UserM,status_code=201)
+def update_user(id: int,user: UserM)-> UserM:
+    db = Session()
+    result = db.query(UserModel).where(UserModel.id == id).first()
+
+    if result:
+        result.name = user.name
+        result.lastname = user.lastname
+        result.age = user.age
+        result.email = user.email
+        result.password = user.password
+        result.address = user.address
+        db.commit()
+        return JSONResponse(status_code=201,content={"message":"user updated succesfully"}) 
+    if not result:
+        return JSONResponse(status_code=404,content={"message":"user not found"})
+
+#creamos el endpoint que nos permitira eliminar un usuario
+@app.delete("/users/{id}",tags=['Users'],response_model= UserM,status_code=201)
+def delete_user(id: int)-> UserM:
+    db = Session()
+    result = db.query(UserModel).where(UserModel.id == id).first()
+    if result:
+        db.delete(result)
+        db.commit()
+        return JSONResponse(status_code=201,content={"message":"user deleted succesfully"})
+    if not result:
+        return JSONResponse(status_code=404,content={"message":"user not found"})
