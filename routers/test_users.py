f7596b9e28b57b79624e7b392868a281e775a5dd
Charbrown08 <cartam15@hotmail.com>
2023-10-10 10:04:40 -0500

    upload test

--- routers/test_users.py
+++ routers/test_users.py
@@ -18,19 +18,7 @@ def sample_user():
     }
 
 
-def test_create_user():
-    response = client.post("/users", json=sample_user())
-    assert response.status_code == 200
-    assert "User con ID" in response.json()["mensaje"]
-
-
 def test_get_users():
     response = client.get("/users")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
-
-
-def test_get_user_by_id():
-    response = client.get("/users/1")
-    assert response.status_code == 404
-    assert "name" in response.json()
